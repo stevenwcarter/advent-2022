80f2ab112eb5fef583f9395919b8896b86215293
Steven Carter <steven.carter@bounteous.com>
2022-12-01 09:23:10 -0500

    Day 1

---
+++ src/bin/1a.rs
@@ -0,0 +1,89 @@
+use advent::read_lines;
+
+fn main() {
+    let puzzle_data = "puzzles/1a.txt";
+
+    let max = a(&puzzle_data);
+    println!("Max calories carried is: {}", max);
+
+    let b_result = b(&puzzle_data);
+    println!("Total carried by top three is: {}", b_result);
+}
+
+fn a(path: &str) -> u32 {
+    let lines = read_lines(path).unwrap();
+
+    let mut elves: Vec<Vec<u32>> = Vec::new();
+
+    let mut elf: Vec<u32> = Vec::new();
+    for line in lines {
+        if line.trim().eq("") {
+            elves.push(elf.clone());
+            elf = Vec::new();
+        } else {
+            let calories: u32 = line.parse().unwrap();
+            elf.push(calories);
+        }
+    }
+    // push last elf since no blank line after
+    elves.push(elf);
+
+    let mut max = 0;
+    for elf in elves {
+        let total = elf.iter().sum();
+        if total > max {
+            max = total;
+        }
+    }
+
+    max
+}
+
+fn b(path: &str) -> u32 {
+    let lines = read_lines(path).unwrap();
+
+    let mut elves: Vec<Vec<u32>> = Vec::new();
+
+    let mut elf: Vec<u32> = Vec::new();
+    for line in lines {
+        if line.trim().eq("") {
+            elves.push(elf.clone());
+            elf = Vec::new();
+        } else {
+            let calories: u32 = line.parse().unwrap();
+            elf.push(calories);
+        }
+    }
+    // push last elf since no blank line after
+    elves.push(elf);
+
+    let mut elves_totaled: Vec<u32> = Vec::new();
+    for elf in elves {
+        let total = elf.iter().sum();
+        elves_totaled.push(total);
+    }
+
+    elves_totaled.sort();
+    elves_totaled.reverse();
+
+    elves_totaled[0..3].iter().sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_a() {
+        let test_path = "test-resources/day1.txt";
+        let result = a(&test_path);
+        assert_eq!(result, 24000);
+    }
+
+    #[test]
+    fn test_b() {
+        let test_path = "test-resources/day1.txt";
+        let result = b(&test_path);
+        assert_eq!(result, 45000);
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+pub fn read_lines<P>(filename: P) -> io::Result<Vec<String>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+
+    let reader = io::BufReader::new(file).lines();
+    let lines: Vec<String> = reader.into_iter().map(|l| l.unwrap()).collect();
+
+    Ok(lines)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_reads_lines_from_file() {
+        let result = read_lines("test-resources/test.txt").unwrap();
+        assert_eq!(result.len(), 3);
+        assert_eq!(result[2], "c");
+    }
+}
