298722a915879b4e748fc509b8a0eccea885a1a1
Steven Carter <steven.carter@bounteous.com>
2022-12-05 10:14:57 -0500

    Cleaning up solution

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -1,103 +1,77 @@
 use advent::read_lines;
 
+fn build_stacks() -> Vec<Vec<char>> {
+    let stacks: Vec<Vec<char>> = vec![
+        "RPCDBG".chars().collect(),
+        "HVG".chars().collect(),
+        "NSQDJPM".chars().collect(),
+        "PSLGDCNM".chars().collect(),
+        "JBNCPFLS".chars().collect(),
+        "QBDZVGTS".chars().collect(),
+        "BZMHFTQ".chars().collect(),
+        "CMDBF".chars().collect(),
+        "FCQG".chars().collect(),
+    ];
+
+    stacks
+}
+
 fn main() {
     let puzzle_data = "puzzles/5.txt";
 
-    let mut stacks: Vec<Vec<char>> = Vec::new();
-    let stack1 = vec!['R', 'P', 'C', 'D', 'B', 'G'];
-    let stack2 = vec!['H', 'V', 'G'];
-    let stack3: Vec<char> = "NSQDJPM".chars().collect();
-    let stack4: Vec<char> = "PSLGDCNM".chars().collect();
-    let stack5: Vec<char> = "JBNCPFLS".chars().collect();
-    let stack6: Vec<char> = "QBDZVGTS".chars().collect();
-    let stack7: Vec<char> = "BZMHFTQ".chars().collect();
-    let stack8: Vec<char> = "CMDBF".chars().collect();
-    let stack9: Vec<char> = "FCQG".chars().collect();
-
-    stacks.push(stack1);
-    stacks.push(stack2);
-    stacks.push(stack3);
-    stacks.push(stack4);
-    stacks.push(stack5);
-    stacks.push(stack6);
-    stacks.push(stack7);
-    stacks.push(stack8);
-    stacks.push(stack9);
+    let mut stacks = build_stacks();
 
     let answer_a = a(puzzle_data, &mut stacks);
-    println!("Total completely overlapping is: {}", answer_a);
+    println!("Order of top crates (1-by-1): {}", answer_a);
 
-    let mut stacks: Vec<Vec<char>> = Vec::new();
-    let stack1 = vec!['R', 'P', 'C', 'D', 'B', 'G'];
-    let stack2 = vec!['H', 'V', 'G'];
-    let stack3: Vec<char> = "NSQDJPM".chars().collect();
-    let stack4: Vec<char> = "PSLGDCNM".chars().collect();
-    let stack5: Vec<char> = "JBNCPFLS".chars().collect();
-    let stack6: Vec<char> = "QBDZVGTS".chars().collect();
-    let stack7: Vec<char> = "BZMHFTQ".chars().collect();
-    let stack8: Vec<char> = "CMDBF".chars().collect();
-    let stack9: Vec<char> = "FCQG".chars().collect();
-
-    stacks.push(stack1);
-    stacks.push(stack2);
-    stacks.push(stack3);
-    stacks.push(stack4);
-    stacks.push(stack5);
-    stacks.push(stack6);
-    stacks.push(stack7);
-    stacks.push(stack8);
-    stacks.push(stack9);
+    let mut stacks = build_stacks();
 
     let b_result = b(puzzle_data, &mut stacks);
-    println!("Total somewhat overlapping is: {}", b_result);
+    println!("Order of top crates (total-move): {}", b_result);
+}
+
+fn parse_line(line: &str) -> (usize, usize, usize) {
+    let parts: Vec<String> = line.split(' ').map(|s| s.to_string()).collect();
+    let quantity: usize = parts[1].parse::<usize>().unwrap();
+    let source: usize = parts[3].parse::<usize>().unwrap() - 1;
+    let dest: usize = parts[5].parse::<usize>().unwrap() - 1;
+
+    (quantity, source, dest)
+}
+
+fn parse_result(stacks: &mut [Vec<char>]) -> String {
+    let result: String = stacks.iter().map(|s| s.last().unwrap()).collect();
+
+    result
 }
 
 fn a(path: &str, stacks: &mut [Vec<char>]) -> String {
-    let lines = read_lines(path).unwrap();
+    read_lines(path)
+        .unwrap()
+        .iter()
+        .filter(|l| l.contains("move"))
+        .map(|line| parse_line(line))
+        .for_each(|(quantity, source, dest)| {
+            let mut crates_to_move = stacks[source].split_off(stacks[source].len() - quantity);
+            crates_to_move.reverse();
+            stacks[dest].append(&mut crates_to_move);
+        });
 
-    for line in lines {
-        if line.contains("move") {
-            let parts: Vec<String> = line.split(' ').map(|s| s.to_string()).collect();
-            let quantity: u32 = parts[1].parse::<u32>().unwrap();
-            let source: u32 = parts[3].parse::<u32>().unwrap() - 1;
-            let dest: u32 = parts[5].parse::<u32>().unwrap() - 1;
-
-            for _ in 0..quantity {
-                let char = stacks[source as usize].pop().unwrap();
-                stacks[dest as usize].push(char);
-            }
-        }
-    }
-
-    let result: String = stacks.iter().map(|s| s.last().unwrap()).collect();
-
-    result
+    parse_result(stacks)
 }
 
 fn b(path: &str, stacks: &mut [Vec<char>]) -> String {
-    let lines = read_lines(path).unwrap();
+    read_lines(path)
+        .unwrap()
+        .iter()
+        .filter(|l| l.contains("move"))
+        .map(|line| parse_line(line))
+        .for_each(|(quantity, source, dest)| {
+            let mut crates_to_move = stacks[source].split_off(stacks[source].len() - quantity);
+            stacks[dest].append(&mut crates_to_move);
+        });
 
-    for line in lines {
-        if line.contains("move") {
-            let parts: Vec<String> = line.split(' ').map(|s| s.to_string()).collect();
-            let quantity: u32 = parts[1].parse::<u32>().unwrap();
-            let source: u32 = parts[3].parse::<u32>().unwrap() - 1;
-            let dest: u32 = parts[5].parse::<u32>().unwrap() - 1;
-
-            let mut crates: Vec<char> = Vec::new();
-            for _ in 0..quantity {
-                crates.push(stacks[source as usize].pop().unwrap());
-            }
-            crates.reverse();
-            for _ in 0..quantity {
-                stacks[dest as usize].append(&mut crates);
-            }
-        }
-    }
-
-    let result: String = stacks.iter().map(|s| s.last().unwrap()).collect();
-
-    result
+    parse_result(stacks)
 }
 
 #[cfg(test)]
@@ -120,6 +94,14 @@ mod tests {
     }
 
     #[test]
+    fn test_a_final() {
+        let mut stacks = build_stacks();
+        let path = "puzzles/5.txt";
+        let result = a(path, &mut stacks);
+        assert_eq!(result, "TLNGFGMFN");
+    }
+
+    #[test]
     fn test_b() {
         let mut stacks: Vec<Vec<char>> = Vec::new();
         let stack1: Vec<char> = "ZN".chars().collect();
@@ -132,5 +114,13 @@ mod tests {
         let test_path = "test-resources/day5.txt";
         let result = b(test_path, &mut stacks);
         assert_eq!(result, "MCD");
+    }
+
+    #[test]
+    fn test_b_final() {
+        let mut stacks = build_stacks();
+        let path = "puzzles/5.txt";
+        let result = b(path, &mut stacks);
+        assert_eq!(result, "FGLQJCMBD");
     }
 }
