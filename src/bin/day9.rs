d0cdde76064558597425284a0c66dd03e7d860ec
Steven Carter <steven.carter@bounteous.com>
2022-12-09 14:21:20 -0500

    Day 9 - cleanup?

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -6,97 +6,86 @@ fn main() {
     let puzzle_data = "puzzles/9.txt";
 
     let (answer_a, answer_b) = a(puzzle_data);
-    println!("A Score: {}", answer_a);
 
+    println!("A Score: {}", answer_a);
     println!("B Score: {}", answer_b);
 }
 
-fn distance_too_great_and_adjust(hx: &i32, hy: &i32, tx: &mut i32, ty: &mut i32) {
-    if *hx - *tx > 1 {
-        if *hy - *ty > 1 {
-            *ty = *hy - 1;
-        } else if *hy - *ty < -1 {
-            *ty = *hy + 1;
+fn distance_too_great_and_adjust(i: usize, x: &mut [i32], y: &mut [i32]) {
+    if x[i] - x[i + 1] > 1 {
+        if y[i] - y[i + 1] > 1 {
+            y[i + 1] = y[i] - 1;
+        } else if y[i] - y[i + 1] < -1 {
+            y[i + 1] = y[i] + 1;
         } else {
-            *ty = *hy;
+            y[i + 1] = y[i];
         }
-        *tx = *hx - 1;
-    } else if *hx - *tx < -1 {
-        if *hy - *ty > 1 {
-            *ty = *hy - 1;
-        } else if *hy - *ty < -1 {
-            *ty = *hy + 1;
+        x[i + 1] = x[i] - 1;
+    } else if x[i] - x[i + 1] < -1 {
+        if y[i] - y[i + 1] > 1 {
+            y[i + 1] = y[i] - 1;
+        } else if y[i] - y[i + 1] < -1 {
+            y[i + 1] = y[i] + 1;
         } else {
-            *ty = *hy;
+            y[i + 1] = y[i];
         }
-        *tx = *hx + 1;
-    } else if *hy - *ty > 1 {
-        if *hx - *tx > 1 {
-            *tx = *hx - 1;
-        } else if *hx - *tx < -1 {
-            *tx = *hx + 1;
+        x[i + 1] = x[i] + 1;
+    } else if y[i] - y[i + 1] > 1 {
+        if x[i] - x[i + 1] > 1 {
+            x[i + 1] = x[i] - 1;
+        } else if x[i] - x[i + 1] < -1 {
+            x[i + 1] = x[i] + 1;
         } else {
-            *tx = *hx;
+            x[i + 1] = x[i];
         }
-        *tx = *hx;
-        *ty = *hy - 1;
-    } else if *hy - *ty < -1 {
-        if *hx - *tx > 1 {
-            *tx = *hx - 1;
-        } else if *hx - *tx < -1 {
-            *tx = *hx + 1;
+        x[i + 1] = x[i];
+        y[i + 1] = y[i] - 1;
+    } else if y[i] - y[i + 1] < -1 {
+        if x[i] - x[i + 1] > 1 {
+            x[i + 1] = x[i] - 1;
+        } else if x[i] - x[i + 1] < -1 {
+            x[i + 1] = x[i] + 1;
         } else {
-            *tx = *hx;
+            x[i + 1] = x[i];
         }
-        *tx = *hx;
-        *ty = *hy + 1;
+        x[i + 1] = x[i];
+        y[i + 1] = y[i] + 1;
     }
 }
 
 fn a(path: &str) -> (usize, usize) {
-    let (mut hx, mut hy): (i32, i32) = (0, 0);
-    let (mut tx1, mut ty1): (i32, i32) = (0, 0);
-    let (mut tx2, mut ty2): (i32, i32) = (0, 0);
-    let (mut tx3, mut ty3): (i32, i32) = (0, 0);
-    let (mut tx4, mut ty4): (i32, i32) = (0, 0);
-    let (mut tx5, mut ty5): (i32, i32) = (0, 0);
-    let (mut tx6, mut ty6): (i32, i32) = (0, 0);
-    let (mut tx7, mut ty7): (i32, i32) = (0, 0);
-    let (mut tx8, mut ty8): (i32, i32) = (0, 0);
-    let (mut tx9, mut ty9): (i32, i32) = (0, 0);
+    // starting positions
+    let mut tx_vec: Vec<i32> = vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    let mut ty_vec: Vec<i32> = vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
+    // keep track of where two of the knots "visit"
     let mut visited_a: HashSet<(i32, i32)> = HashSet::new();
     let mut visited_b: HashSet<(i32, i32)> = HashSet::new();
 
     read_lines(path).unwrap().iter().for_each(|line| {
         let mut chars = line.chars();
         let direction: char = chars.next().unwrap();
-        chars.next();
+        chars.next(); // skip the space
         let remaining: String = chars.collect();
         let distance: i32 = remaining.parse::<i32>().unwrap();
 
         for _ in 0..distance {
             if direction == 'U' {
-                hy += 1;
+                ty_vec[0] += 1;
             } else if direction == 'D' {
-                hy -= 1;
+                ty_vec[0] -= 1;
             } else if direction == 'L' {
-                hx -= 1;
+                tx_vec[0] -= 1;
             } else {
-                hx += 1;
+                tx_vec[0] += 1;
             }
 
-            distance_too_great_and_adjust(&hx, &hy, &mut tx1, &mut ty1);
-            distance_too_great_and_adjust(&tx1, &ty1, &mut tx2, &mut ty2);
-            distance_too_great_and_adjust(&tx2, &ty2, &mut tx3, &mut ty3);
-            distance_too_great_and_adjust(&tx3, &ty3, &mut tx4, &mut ty4);
-            distance_too_great_and_adjust(&tx4, &ty4, &mut tx5, &mut ty5);
-            distance_too_great_and_adjust(&tx5, &ty5, &mut tx6, &mut ty6);
-            distance_too_great_and_adjust(&tx6, &ty6, &mut tx7, &mut ty7);
-            distance_too_great_and_adjust(&tx7, &ty7, &mut tx8, &mut ty8);
-            distance_too_great_and_adjust(&tx8, &ty8, &mut tx9, &mut ty9);
-            visited_a.insert((tx1, ty1));
-            visited_b.insert((tx9, ty9));
+            for i in 0..9 {
+                distance_too_great_and_adjust(i, &mut tx_vec, &mut ty_vec);
+            }
+
+            visited_a.insert((tx_vec[1], ty_vec[1]));
+            visited_b.insert((tx_vec[9], ty_vec[9]));
         }
     });
 
@@ -112,13 +101,6 @@ mod tests {
         let test_path = "test-resources/day9.txt";
         let (result, _) = a(test_path);
         assert_eq!(result, 13);
-    }
-
-    #[test]
-    fn test_internal() {
-        let (x, y, mut a, mut b): (i32, i32, i32, i32) = (4, 3, 2, 5);
-        distance_too_great_and_adjust(&x, &y, &mut a, &mut b);
-        assert_eq!((a, b), (3, 3));
     }
 
     #[test]
